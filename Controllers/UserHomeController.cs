e47eefcce560c7f0f053d8e0fd66149c18003685
nushirvannaseer@gmail.com <nushirvannaseer@gmail.com>
2020-05-30 01:06:10 +0500

    Added the redirect to session expired page in case session expires

--- Controllers/UserHomeController.cs
+++ Controllers/UserHomeController.cs
@@ -19,31 +19,39 @@ namespace Wembsite.Controllers
         public ActionResult Profile()
         {
             if (Session["username"] == null)
-                return RedirectToAction("../Login/Index");
+                return RedirectToAction("SessionExpired");
             User user = CRUD.getUser(Session["username"].ToString());
             return View(user);
         }
 
         public ActionResult Followers()
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<User> users = CRUD.DisplayFollowersOfAUser(Session["username"].ToString());
             return View("Followers", users);
         }
 
         public ActionResult Following()
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<User> users = CRUD.DisplayFollowingsOfAUser(Session["username"].ToString());
             return View("Following", users);
         }
 
         public ActionResult FollowRequests()
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<User> users = CRUD.DisplayFollowRequestsOfAUser(Session["username"].ToString());
             return View("FollowRequests", users);
         }
 
         public ActionResult SendFollowRequest(string usernameB, string source)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.SendFollowRequest(Session["username"].ToString(), usernameB);
             if (source == "../AllUsers/NonSessionUserProfile")
                 return View(source, CRUD.getUser(usernameB));
@@ -53,6 +61,8 @@ namespace Wembsite.Controllers
 
         public ActionResult CancelFollowRequest(string usernameB, string source)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.CancelFollowRequest(Session["username"].ToString(), usernameB);
             if (source == "../AllUsers/NonSessionUserProfile")
                 return View(source, CRUD.getUser(usernameB));
@@ -62,6 +72,8 @@ namespace Wembsite.Controllers
 
         public ActionResult AcceptFollowRequest(string sender, string source)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.AcceptFollowRequest(sender, Session["username"].ToString());
             if (source == "FollowRequests")
             {
@@ -75,6 +87,8 @@ namespace Wembsite.Controllers
 
         public ActionResult DeleteFollowRequest(string sender, string source)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.CancelFollowRequest(sender, Session["username"].ToString());
             if (source == "FollowRequests")
             {
@@ -93,6 +107,8 @@ namespace Wembsite.Controllers
 
         public ActionResult DeleteFollower(string followee, string source)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.DeleteFollower(followee, Session["username"].ToString());
             if (source == "../AllUsers/NonSessionUserProfile")
             {
@@ -109,6 +125,8 @@ namespace Wembsite.Controllers
 
         public ActionResult PublishPost(string postContent, string privacy, HttpPostedFileBase file)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             if (file != null && file.ContentLength > 0)
             {
                 string fileName = Session["username"].ToString() + "__" + file.FileName;
@@ -136,36 +154,48 @@ namespace Wembsite.Controllers
 
         public ActionResult AllPosts()
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<UserContent> postList = CRUD.AllPostsOfAUser(Session["username"].ToString());
             return View(postList);
         }
 
         public ActionResult ViewPost(int id)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             UserContent post = CRUD.GetUserPost(id);
             return View(post);
         }
 
         public ActionResult EditPost(int id)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             UserContent post = CRUD.GetUserPost(id);
             return View(post);
         }
 
         public ActionResult SaveEditChanges(int id, string privacy, string RawData)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.EditPost(id, privacy, RawData);
             return RedirectToAction("AllPosts");
         }
 
         public ActionResult DeletePost(int id)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             CRUD.DeletePost(id);
             return RedirectToAction("AllPosts");
         }
 
         public ActionResult HomePage()
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<UserContent> postList = CRUD.HomepagePost(Session["username"].ToString());
 
             return View(postList);
@@ -173,6 +203,8 @@ namespace Wembsite.Controllers
 
         public ActionResult LikePost(int contentID, string likedBy)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             int originalLikes = Convert.ToInt32(CRUD.GetUserPost(contentID).likes);
             CRUD.LikePost(contentID, likedBy);
             ViewData["Message"] = "Unlike";
@@ -188,18 +220,24 @@ namespace Wembsite.Controllers
 
         public ActionResult GetLikeList(int contentID)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<User> likers = CRUD.GetLikeList(contentID);
             return PartialView("_LikeListPartialView", likers);
         }
 
         public ActionResult _NonSessionUserPosts(string username)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             List<UserContent> posts = CRUD.getNonSessionUserPosts(username, Session["username"].ToString());
             return View(posts);
         }
 
         public ActionResult Search(string searchText)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
             if (searchText == "")
             {
                 return RedirectToAction("Profile");
@@ -211,15 +249,26 @@ namespace Wembsite.Controllers
 
         public ActionResult AddComment(string commentText, string contentID)
         {
+            if (Session["username"] == null)
+                return RedirectToAction("SessionExpired");
+            
             var cID = ""; int i = 0;
             while (contentID[i] >= 48 && contentID[i] <= 57)
             {
                 cID += contentID[i];
                 i++;
             }
-            CRUD.AddComment(Convert.ToInt32(cID), Session["username"].ToString(), commentText);
             ViewData["ContentID"] = cID;
+            if (commentText == "" || commentText==null)
+                return PartialView("_Comment", CRUD.GetCommentsOfAPost(Convert.ToInt32(cID)));
+
+            CRUD.AddComment(Convert.ToInt32(cID), Session["username"].ToString(), commentText);
             return PartialView("_Comment", CRUD.GetCommentsOfAPost(Convert.ToInt32(cID)));
+        }
+
+        public ActionResult SessionExpired()
+        {
+            return View();
         }
     }
 }
